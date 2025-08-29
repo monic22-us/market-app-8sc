a556d217654c0a6e3d5cd1c4ba4c28a8f6187ba0
monic22-us <monsemorales367@gmail.com>
2025-08-28 18:55:00 -0600

    first commit

---
+++ src/main/java/com/itmerida/market_app_8sc/MarketApp8scApplication.java
@@ -0,0 +1,13 @@
+package com.itmerida.market_app_8sc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketApp8scApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketApp8scApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itmerida/market_app_8sc/MarketApp8scApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itmerida.market_app_8sc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketApp8scApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
