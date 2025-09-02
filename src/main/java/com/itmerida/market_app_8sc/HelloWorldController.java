46d9230fa38e095b43f4ba819b88a929c4c94048
monic22-us <monsemorales367@gmail.com>
2025-09-02 17:27:53 -0600

    segunda clase

--- src/main/java/com/itmerida/market_app_8sc/HelloWorldController.java
+++ src/main/java/com/itmerida/market_app_8sc/HelloWorldController.java
@@ -1,16 +1,16 @@
 package com.itmerida.market_app_8sc;
 
-
 import org.springframework.web.bind.annotation.GetMapping;
-import  org.springframework.web.bind.annotation.RequestMaping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/hola")
+public class HelloWorldController {
 
-@RestController new *
-    @RestController ("/hola")
-public class HelloWorldController{
     @GetMapping("/saludar")
     public String saludar (){
-        return "Hello World";
-}
+        return "¡Hola mundito!";
+
+    }
 }
